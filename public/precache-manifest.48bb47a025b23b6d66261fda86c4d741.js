9bed250d516ec8971280dd50722dddacb237e66b
jorgea306 <jorgea306@gmail.com>
2020-09-20 00:40:39 -0300

    v1 CalendarApp

---
+++ public/precache-manifest.48bb47a025b23b6d66261fda86c4d741.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9f12305cc7fcce1c936a7cbf222241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2b171074fdcf2010a6",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "4fe42c863664857d416d",
+    "url": "/static/css/main.c2f9d8e7.chunk.css"
+  },
+  {
+    "revision": "3c2b171074fdcf2010a6",
+    "url": "/static/js/2.c2615479.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.c2615479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe42c863664857d416d",
+    "url": "/static/js/main.d5fbdd1a.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
