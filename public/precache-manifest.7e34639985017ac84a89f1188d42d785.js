888e4fdac2b9029b01384c1cc2d3208dd6b9ae90
jorgea306 <jorgea306@gmail.com>
2020-09-21 22:11:01 -0300

    v10 calendar

---
+++ public/precache-manifest.7e34639985017ac84a89f1188d42d785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af3220eb296a0f7c207887ea260b5f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c2b171074fdcf2010a6",
+    "url": "/static/css/2.dbd11c43.chunk.css"
+  },
+  {
+    "revision": "57b9685c8bcbee29965c",
+    "url": "/static/css/main.c2f9d8e7.chunk.css"
+  },
+  {
+    "revision": "3c2b171074fdcf2010a6",
+    "url": "/static/js/2.c2615479.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.c2615479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b9685c8bcbee29965c",
+    "url": "/static/js/main.98015774.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
